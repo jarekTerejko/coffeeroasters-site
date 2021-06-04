66ee465fed0f3f4be83a7c69a2cb74af18dc2fa4
jarekTerejko <j.terejko@gmail.com>
2021-06-04 05:12:46 +0200

    create Container

---
+++ src/components/Container/ContainerElement.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ContainerElement = styled.div`
+  max-width: ${(props) => (props.wider ? "132rem" : "115rem")};
+  padding: 0 2rem;
+  border-radius: ${(props) => (props.wider ? "2rem" : "")};
+  margin: 0 auto;
+`;
