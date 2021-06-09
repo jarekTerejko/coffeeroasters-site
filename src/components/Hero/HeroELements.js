c9bf2ee56d2a1033b8c4d24eecd502c5ffbc4a84
jarekTerejko <j.terejko@gmail.com>
2021-06-09 13:11:14 +0200

    add media queries

--- src/components/Hero/HeroELements.js
+++ src/components/Hero/HeroELements.js
@@ -1,13 +1,35 @@
 import styled from "styled-components";
 
-export const HeroWrapper = styled.header``;
+export const HeroWrapper = styled.header`
+  margin-bottom: 16rem;
+  margin-top: 11.4rem;
+`;
 
 export const HeroInnerWrapper = styled.div`
   background: ${(props) => (props.HeroBgD ? `url(${props.HeroBgD})` : "black")};
-  margin-top: 11.4rem;
   min-height: 60rem;
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   border-radius: 1rem;
+  padding: 12rem 0 10rem 0;
+
+  @media screen and (max-width: 768px) {
+    padding: 10rem 0 8rem 0;
+    background: ${(props) =>
+      props.HeroBgT ? `url(${props.HeroBgT})` : "black"};
+    background-repeat: no-repeat;
+    background-position: right;
+    background-size: cover;
+    min-height: 50rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    background: ${(props) =>
+      props.HeroBgM ? `url(${props.HeroBgM})` : "black"};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    text-align: center;
+  }
 `;
