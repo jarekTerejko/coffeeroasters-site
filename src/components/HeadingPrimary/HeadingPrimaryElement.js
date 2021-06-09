026b3b19ed6ab1a5442910793dda98ea0aae9917
jarekTerejko <j.terejko@gmail.com>
2021-06-09 13:06:50 +0200

    add media queries

--- src/components/HeadingPrimary/HeadingPrimaryElement.js
+++ src/components/HeadingPrimary/HeadingPrimaryElement.js
@@ -4,8 +4,17 @@ export const HeadingPrimary = styled.h1`
   color: var(--lightCream);
   font-size: 6.4rem;
   font-family: var(--serif);
-  padding-top: ${(props) => (props.homepageHero ? "10rem" : "")};
   max-width: 50rem;
   margin-bottom: 3rem;
   font-weight: var(--weight-bold);
+
+  @media screen and (max-width: 768px) {
+    font-size: 4rem;
+    max-width: 34rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    font-size: 3.4rem;
+    max-width: 28rem;
+  }
 `;
