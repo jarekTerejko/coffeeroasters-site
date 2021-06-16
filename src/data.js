0452967485ac63fe99faf6c283e8ae1414771c07
jarekTerejko <j.terejko@gmail.com>
2021-06-16 08:39:08 +0200

    add data for HowItWorks Section

--- src/data.js
+++ src/data.js
@@ -94,6 +94,24 @@ export const whyChooseCards = [
   },
 ];
 
+export const howItWorks = [
+  {
+    heading: "Pick your coffee",
+    description:
+      "Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for hem. There are new coffees in all profiles every month for you to try out.",
+  },
+  {
+    heading: "Choose the frequency",
+    description:
+      "Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your	subscription with no commitment through our online portal.",
+  },
+  {
+    heading: "Receive and enjoy!",
+    description:
+      "We ship your package within 48 hours, freshly roasted.	Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.",
+  },
+];
+
 export const headquarters = [
   {
     icon: ukIcon,
