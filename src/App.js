4adfe4fe3d2073df91c7ed039731c8e99552ab7d
jarekTerejko <j.terejko@gmail.com>
2021-06-21 08:51:43 +0200

    add AboutUs page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import { Route, Switch } from "react-router";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
+
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -31,6 +33,9 @@ const App = () => {
       <Switch>
         <Route path="/" exact>
           <Home />
+        </Route>
+        <Route path="/about-us" exact>
+          <AboutUs/>
         </Route>
       </Switch>
       <Footer />
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import HeroPages from "../components/HeroPages"
+import OurCommitment from "../components/OurCommitmentSection"
+
+const AboutUs = () => {
+    return ( <>
+        <HeroPages/>
+        <OurCommitment />
+    </> );
+}
+ 
+export default AboutUs;
