fcd5a1477e64e38a4b359ebe6483311ca21ab656
jarekTerejko <j.terejko@gmail.com>
2021-07-31 05:51:49 +0200

    add prop to style specific image in OurHeadquarters component

--- src/components/Image/ImageElement.js
+++ src/components/Image/ImageElement.js
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
-export const ImageEl = styled.img``;
+export const ImageEl = styled.img`
+  margin-bottom: ${(props) => (props.ourHeadquarters ? "4rem" : "")};
+`;
